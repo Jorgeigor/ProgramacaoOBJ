aaea68e9cfa7e042103e102ef09234dadfadf1bf
JORGE IGOR BARROS GOMES <jorge.gomes@a.ucb.br>
2023-08-11 10:43:06 -0300

    adiciona pasta

---
+++ atividade1/verificaNumeroPrimo.java
@@ -0,0 +1,15 @@
+package atividade1;
+
+/**
+ * verificaNumeroPrimo
+ */
+public class verificaNumeroPrimo {
+
+    public static void main(String args[]){
+        System.out.println ("Olá, mundo");
+        verificaNumeroPrimo (9)
+    }
+    public static void verificaNumeroPrimo(Interger n){
+
+    }
+}
