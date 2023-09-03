e89330cf0f6d3e460abfece8d404b0e55e063ef7
Jorgeigor <142018986+Jorgeigor@users.noreply.github.com>
2023-09-03 15:46:53 -0300

    Update verificaNumeroPrimo.java

--- atividade1/verificaNumeroPrimo.java
+++ atividade1/verificaNumeroPrimo.java
@@ -1,15 +1,27 @@
 package atividade1;
 
-/**
- * verificaNumeroPrimo
- */
-public class verificaNumeroPrimo {
+public class VerificaNumeroPrimo {
 
-    public static void main(String args[]){
-        System.out.println ("Olá, mundo");
-        verificaNumeroPrimo (9)
+    public static int verificaNumeroPrimo(Integer n) {
+        if (n <= 1) {
+            return 0; // Números menores ou iguais a 1 não são primos
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return 0; // Encontrou um divisor, não é primo
+            }
+        }
+        return 1; // É primo
     }
-    public static void verificaNumeroPrimo(Interger n){
 
+    public static void main(String[] args) {
+        Integer numero = 17; // Substitua pelo número que deseja verificar
+        int resultado = verificaNumeroPrimo(numero);
+        
+        if (resultado == 1) {
+            System.out.println(numero + " é um número primo.");
+        } else {
+            System.out.println(numero + " não é um número primo.");
+        }
     }
 }
